ed57cbd500b1324ad8407f889b578d7862bf7866
kefir <lala@mail.ru>
2020-10-08 15:09:53 +0300

    delete class

---
+++ src/test/java/FirstClass.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("tests")
+public class FirstClass{
+    @Test
+    void firstTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    void secondTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    void thirdTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    void forthTest() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled
+    void fifthTest() {
+        assertTrue(true);
+    }
+
+}
